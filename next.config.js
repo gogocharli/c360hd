df51b9d4c16cb14649658ddaf4fd729a57ece765
Charles De Mount <thexrayone@icloud.com>
2021-03-16 16:55:51 -0400

    feat(styles) :construction: add global base style and css architecture with sass
    - add style loading path for sass
    
    - use gorko for design tokens configuration
    
    - start using google fonts for fonts import

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const { i18n } = require('./next-i18next.config');
+const path = require('path');
 
 module.exports = {
   i18n,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'node_modules/gorko')],
+  },
 };
