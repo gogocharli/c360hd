b88071723cc5e329a8979d9a0bdb93f3f49480c1
Charles De Mount <thexrayone@icloud.com>
2021-03-23 19:16:21 -0400

    refactor(markup): :bento: add initial color to accordion icons
    remove module styles for accordion

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -7,8 +7,6 @@ import {
 import PlusIcon from './icon-plus.svg';
 import MinusIcon from './icon-minus.svg';
 
-import * as styles from './styles.module.css';
-
 function Menu({ children }: { children: React.ReactNode }) {
   return <Accordion collapsible>{children}</Accordion>;
 }
@@ -21,20 +19,18 @@ function Item({
   item: { value: string; title: string };
 }) {
   return (
-    <>
-      <AccordionItem className={styles.accordionItem}>
-        <h3>
-          <AccordionButton className={styles.accordionButton}>
-            {item.title}
-            <div className='accordion__icon' aria-hidden='true'>
-              <PlusIcon className={styles.icon} width={24} />
-              <MinusIcon className={styles.icon} width={24} />
-            </div>
-          </AccordionButton>
-        </h3>
-        <AccordionPanel>{children}</AccordionPanel>
-      </AccordionItem>
-    </>
+    <AccordionItem className='accordion__item'>
+      <h3>
+        <AccordionButton className='accordion__button'>
+          {item.title}
+          <div className='accordion__icon' aria-hidden='true'>
+            <PlusIcon className='icon__open' width={24} />
+            <MinusIcon className='icon__close' width={24} />
+          </div>
+        </AccordionButton>
+      </h3>
+      <AccordionPanel className='accordion__content'>{children}</AccordionPanel>
+    </AccordionItem>
   );
 }
 
