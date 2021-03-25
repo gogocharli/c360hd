663bd5c3d88b63525965fe7618326b9555873a7e
Charles De Mount <thexrayone@icloud.com>
2021-03-25 15:20:33 -0400

    feat(markup): :memo: wire up search and category filter using Link component

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -7,10 +7,37 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import BaseLayout from '@layouts/base';
+import { AccordionMenu, AccordionItem } from '@components/Accordion';
+import { useEffect, useState } from 'react';
 
 export default function Portfolio() {
   const { t } = useTranslation('portfolio');
   const pageMeta = { title: t('pageTitle'), desc: t('pageDesc') };
+
+  const { search: filter } = typeof window !== 'undefined' && window?.location;
+  const category = new URLSearchParams(filter)?.get('filter');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    console.log(category);
+  }, [category]);
+
+  function handleSearchChange(e) {
+    const value = e.target.value;
+    console.log(value);
+    setSearchQuery(value);
+  }
+
+  const categoryList = [
+    'resto',
+    'fashion',
+    'convenience',
+    'beauty',
+    'art',
+    'nursery',
+    'recent',
+    'other',
+  ];
 
   return (
     <BaseLayout pageMeta={pageMeta} theme='light'>
@@ -26,6 +53,27 @@ export default function Portfolio() {
           height={417}
         />
       </section>
+      <article>
+        <input
+          type='search'
+          name='search'
+          id='search'
+          aria-label={`${t('search.placeholder')}`}
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <AccordionMenu>
+          <AccordionItem
+            item={{ value: 'filter', title: `${t('categories.title')}` }}
+          >
+            {categoryList.map((category) => (
+              <Link href={`/portfolio?filter=${category}`} key={category}>
+                <a>{t(`categories.${category}`)}</a>
+              </Link>
+            ))}
+          </AccordionItem>
+        </AccordionMenu>
+      </article>
     </BaseLayout>
   );
 }
