cd2aea95753a3f8e3f290d81e70800cae0c1430f
Charles De Mount <thexrayone@icloud.com>
2021-03-29 15:20:06 -0400

    feat(markup): :memo: add error screen for gallery

--- src/components/Gallery/gallery.tsx
+++ src/components/Gallery/gallery.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useEffect, useReducer } from 'react';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
@@ -35,6 +36,17 @@ export function Gallery() {
       {isIdle && <InitialRows items={state.items} />}
       {isFiltered && (
         <GalleryRow category={state.filter} itemList={state.items} all />
+      )}
+      {isNotFound && (
+        <div className='error'>
+          <Image
+            src='/images/magnifier-img.png'
+            alt=''
+            width={48}
+            height={48}
+          />
+          <p>{t('search.noResults')}</p>
+        </div>
       )}
       <Link href='/portfolio?filter=all'>
         <a>{t('buttonText')}</a>
