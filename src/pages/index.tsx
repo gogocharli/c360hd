cd6cbe0172b5d1e9baed93dc3774d21b0ae4e388
Charles De Mount <thexrayone@icloud.com>
2021-04-04 23:30:03 -0400

    feat(styles): :lipstick: add responsive styles for `basics` heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { GetStaticProps } from 'next';
+import Image from 'next/image';
 
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -32,17 +33,43 @@ export default function Home() {
         </article>
         <article id='basics' className='basics'>
           <div className='wrapper'>
-            <h2>
-              <span className='[ subtitle ] [ text-200 md:text-300 weight-normal ] [ tracking-loose upper ]'>
-                {t('sections.1.subtitle')}
-              </span>
-              <span className='[ title ] [ text-550 md:text-600 ] [ leading-flat ]'>
-                {t('sections.1.title')}
-              </span>
-            </h2>
-            <p className='[ text-300 ] [ measure-short ]'>
-              {t('sections.1.desc')}
-            </p>
+            <div className='content'>
+              <h2>
+                <span className='[ subtitle ] [ text-200 md:text-300 weight-normal ] [ tracking-loose upper ]'>
+                  {t('sections.1.subtitle')}
+                </span>
+                <span className='[ title ] [ text-550 md:text-600 ] [ leading-flat ]'>
+                  {t('sections.1.title')}
+                </span>
+              </h2>
+              <p className='[ text-300 ] [ measure-short ]'>
+                {t('sections.1.desc')}
+              </p>
+            </div>
+            <div className='images'>
+              <div className='image'>
+                <Image
+                  src='/images/compass-img.png'
+                  alt=''
+                  width={144}
+                  height={144}
+                  layout='intrinsic'
+                  objectFit='contain'
+                  className='blend'
+                />
+              </div>
+              <div className='image'>
+                <Image
+                  src='/images/flashlight-img.png'
+                  alt=''
+                  width={144}
+                  height={144}
+                  layout='intrinsic'
+                  objectFit='contain'
+                  className='blend'
+                />
+              </div>
+            </div>
           </div>
           <HomeCarousel />
         </article>
@@ -95,6 +122,7 @@ export default function Home() {
         .basics {
           padding-top: 2rem;
           padding-bottom: 2rem;
+          position: relative;
         }
 
         .basics h2 > span {
@@ -103,6 +131,7 @@ export default function Home() {
 
         .basics .title {
           margin-top: 0.5rem;
+          max-width: 15ch;
         }
 
         .basics .subtitle {
@@ -111,6 +140,22 @@ export default function Home() {
 
         .basics p {
           margin-top: 1rem;
+        }
+
+        .basics .image {
+          position: absolute;
+          height: 2rem;
+          width: 2rem;
+        }
+
+        .basics .image:first-of-type {
+          left: 15rem;
+          top: -2rem;
+        }
+
+        .image:last-of-type {
+          right: 1.5rem;
+          top: 6.25rem;
         }
 
         .features {
@@ -148,6 +193,63 @@ export default function Home() {
         @media (min-width: 50em) {
           .journey h2 {
             max-width: 19ch;
+          }
+
+          .basics {
+            padding-bottom: 3rem;
+            padding-top: 3rem;
+          }
+
+          .basics > div:first-of-type {
+            align-items: center;
+            display: flex;
+            justify-content: space-between;
+          }
+
+          .basics p {
+            max-width: 40ch;
+          }
+
+          .basics .images {
+            display: flex;
+            position: relative;
+            flex-basis: 40%;
+            flex-direction: column;
+          }
+
+          .basics .image {
+            position: static;
+            height: 7.5rem;
+            width: 7.5rem;
+          }
+
+          .basics .image:last-of-type {
+            align-self: flex-end;
+          }
+        }
+
+        @media (min-width: 65em) {
+          .basics {
+            padding-bottom: 3.5rem;
+            padding-top: 3.5rem;
+          }
+
+          .basics > div:first-of-type {
+            max-width: min(66vw, 1200px);
+            max-width: clamp(960px, 66vw, 1200px);
+            padding-left: 0;
+            padding-right: 0;
+          }
+
+          .basics .images {
+            max-width: 22rem;
+          }
+        }
+
+        @media (min-width: 90em) {
+          .basics .image {
+            height: 9rem;
+            width: 9rem;
           }
         }
       `}</style>
