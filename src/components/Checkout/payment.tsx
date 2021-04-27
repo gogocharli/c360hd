4f7df71e3be3a937d45ce4ad762b8c3d8912c612
Charles De Mount <thexrayone@icloud.com>
2021-04-27 13:43:19 -0400

    feat(checkout): :construction: add more robust error handling and user feedback for final form submission
    - use new state to prevent multiple subsequent submits since we're using `useEffect`
    
    - set errors using an array of errors for easier handling through the confirmation component
    
    - display the `errorText` when an error occurs server-side

--- src/components/Checkout/payment.tsx
+++ src/components/Checkout/payment.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
-import { SubmitHandler, useFormContext } from 'react-hook-form';
+import {
+  SubmitErrorHandler,
+  SubmitHandler,
+  useFormContext,
+} from 'react-hook-form';
 import { FormInputs } from '../Form/form-field';
+import { ErrorText } from '../Form/error-text';
 import { StripeCheckout } from './stripe-checkout';
 
 export function Payment() {
@@ -14,18 +19,16 @@ export function Payment() {
   } = getValues();
   const [paymentTime, setPaymentTime] = useState<PaymentTime>('idle');
   const [isPaymentSuccess, setPaymentSuccess] = useState(false);
+  const [requestComplete, setRequestComplete] = useState(false);
 
   const [orderNumber, setOrderNumber] = useState('');
   const [isProcessing, setProcessing] = useState(false);
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
 
-  const onSubmit: SubmitHandler<FormInputs> = async (formData) => {
-    // Send the form to /api/orders
-    // Set order number from return data
-    // If there's an error, show it to the user
+  const onValidForm: SubmitHandler<FormInputs> = async (formData) => {
+    const formattedData = { ...formData, date: formData.date.toISOString() };
     try {
-      const formattedData = { ...formData, date: formData.date.toISOString() };
-      const { number: orderNumber } = await window
+      const data = await window
         .fetch('/api/orders', {
           method: 'POST',
           headers: {
@@ -34,18 +37,39 @@ export function Payment() {
           body: JSON.stringify(formattedData),
         })
         .then((res) => res.json());
-      setOrderNumber(orderNumber);
+
+      if (!data.orderNumber) throw data.errorMessage;
+
+      setOrderNumber(data.orderNumber);
+      setErrors([]);
+      setRequestComplete(true); // Avoid making the order when component refreshes
     } catch (err) {
-      console.log(err);
+      console.error(err);
+      setErrors([err]);
+    } finally {
+      setProcessing(false);
     }
   };
 
+  const onInvalidForm: SubmitErrorHandler<FormInputs> = (err) => {
+    setProcessing(false);
+    const errorMessages = Object.entries(err).map(
+      ([field, error]) => `Please verify the ${field}: ${error.message}`,
+    );
+    console.error(errorMessages);
+    setErrors(errorMessages);
+  };
+
   useEffect(() => {
+    if (requestComplete) return;
+
     if (isPaymentSuccess) {
-      handleSubmit(onSubmit)();
+      setProcessing(true);
+      setErrors([]);
+      handleSubmit(onValidForm, onInvalidForm)();
       setPaymentTime(null);
     }
-  }, [isPaymentSuccess]);
+  }, [isPaymentSuccess, requestComplete]);
 
   return (
     <>
@@ -80,8 +104,10 @@ export function Payment() {
       )}
 
       {/* The 'later' option bypasses payment and immediatley places the order */}
-      {(paymentTime == 'later' || isPaymentSuccess) && (
-        <OrderConfirmation orderNumber={orderNumber} />
+      {(paymentTime == 'later' || isPaymentSuccess) && isProcessing ? (
+        <h2>Processing</h2>
+      ) : (
+        <OrderConfirmation orderNumber={orderNumber} errors={errors} />
       )}
 
       <style jsx>{`
@@ -134,8 +160,44 @@ export function Payment() {
   );
 }
 
-function OrderConfirmation({ orderNumber }: { orderNumber: string }) {
-  return <h2 className='align-center'>{orderNumber}</h2>;
+function OrderConfirmation({
+  orderNumber,
+  errors,
+}: {
+  orderNumber: string;
+  errors: any;
+}) {
+  return (
+    <>
+      <div className='order flow'>
+        <h2 className='[ status ] [ text-450 weight-normal leading-flat ]'>
+          {errors.length > 0 ? 'Order Failed' : 'Order Successful'}
+        </h2>
+        {orderNumber && (
+          <p className='[ number ] [ text-700 md:text-800 ] [ weight-bold tracking-tight leading-flat ]'>
+            {orderNumber}
+          </p>
+        )}
+        <p className='[ details ] [ text-300 measure-short ]'>
+          {errors.length > 0
+            ? errors.map((err) => <ErrorText key={err} children={err} />)
+            : `Here’s your order number. We’ve sent you an email with more details.
+          Thanks for doing business with us.`}
+        </p>
+      </div>
+      <style jsx>{`
+        div {
+          --flow-space: 1rem;
+
+          align-items: center;
+          display: flex;
+          flex-direction: column;
+          margin: 0 auto;
+          text-align: center;
+        }
+      `}</style>
+    </>
+  );
 }
 
 function createAdressObj(address: string): Address {
