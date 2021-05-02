b6c9bb525eede7f0f80867639661df2cdec7f43f
Charles De Mount <thexrayone@icloud.com>
2021-05-02 14:11:47 -0400

    fix(markup): :bug: add id provider to make stable ids across ui components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { IdProvider } from '@radix-ui/react-id';
 
 import '../styles/base.scss';
 import '@reach/accordion/styles.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <IdProvider>
+      <Component {...pageProps} />
+    </IdProvider>
+  );
 }
 
 export default appWithTranslation(App);
