af54aa8727e4e692841c04c67e4c52da510862f7
Charles De Mount <thexrayone@icloud.com>
2021-05-08 18:09:50 -0400

    refactor(animtions): :construction: remove complex useEffect callbacks from index page

---
+++ src/lib/theme.ts
@@ -0,0 +1,49 @@
+export function changeThemeOnScrollPosition() {
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (!entry.target) observer.disconnect();
+
+        if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
+          setTheme(bodyEl, 'light');
+        } else {
+          clearTheme(bodyEl);
+        }
+      });
+    },
+    {
+      threshold: [0.25, 0.5, 0.75],
+    },
+  );
+
+  let bodyEl = document.querySelector('body');
+  let targetEl = document.querySelector('#index-hero');
+  observer.observe(targetEl);
+
+  return () => {
+    observer.unobserve(targetEl);
+    // Reset the values to the ones from their respective pages
+    clearTheme(bodyEl);
+  };
+}
+
+const styles = ['bg', 'fg', 'hg', 'tint'];
+const choices = { bg: 'main', fg: 'main', hg: 'highlight', tint: 'tint' };
+function setTheme(element: HTMLElement, theme: 'light' | 'dark') {
+  styles.forEach((style) => {
+    let currentTheme: 'light' | 'dark' = theme;
+    // Revert colors for the foreground color
+    style === 'fg' && (currentTheme = theme == 'dark' ? 'light' : 'dark');
+
+    element.style.setProperty(
+      `--theme-color-${style}`,
+      `var(--color-${currentTheme}-${choices[style]})`,
+    );
+  });
+}
+
+function clearTheme(element: HTMLElement) {
+  styles.forEach((style) =>
+    element.style.setProperty(`--theme-color-${style}`, 'inherit'),
+  );
+}
